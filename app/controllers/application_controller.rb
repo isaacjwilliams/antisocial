5fdf8052135bc18e2dead6074cc2134e11246127
Isaac Williams <isaac@thewilliams.ws>
2014-01-13 17:10:57 -0500

    Fixed Profile Name Display Bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up).concat([:first_name, :last_name, :profile_name])
+  end
 end
--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -69,6 +69,6 @@ class StatusesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def status_params
-      params.require(:status).permit(:name, :content)
+      params.require(:status).permit(:name, :content, :user_id)
     end
 end
