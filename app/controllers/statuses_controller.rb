ef3373a32bf4a5e2f4d3cd19daa856a94869b139
Isaac Williams <isaac@thewilliams.ws>
2014-03-02 16:51:19 -0500

    Require login to creat status

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,4 +1,5 @@
 class StatusesController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
   before_action :set_status, only: [:show, :edit, :update, :destroy]
 
   # GET /statuses
