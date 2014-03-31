9efd9bf0d4284c00d0cc32ab3209f081d0299fae
Isaac Williams <isaac@thewilliams.ws>
2014-03-31 16:57:20 -0400

    Stuff

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -5,7 +5,11 @@ class StatusesController < ApplicationController
   # GET /statuses
   # GET /statuses.json
   def index
-    @statuses = Status.includes(:user).paginate(:page => params[:page], :per_page => 5, :order => "created_at DESC")
+    @statuses = Status.includes(:user).paginate(:page => params[:page], :per_page => 25, :order => "created_at DESC")
+    # respond_to do |format|
+    #   format.html
+    #   format.js
+    # end
   end
 
   # GET /statuses/1
