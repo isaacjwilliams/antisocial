eb72cc60287d9eef5d20310448cb8bc6a3eacbb0
Isaac Williams <isaac@thewilliams.ws>
2014-03-31 14:52:16 -0400

    Added stuff to user_friendship

--- app/models/user_friendship.rb
+++ app/models/user_friendship.rb
@@ -1,5 +1,6 @@
 class UserFriendship < ActiveRecord::Base
 	belongs_to :user
+	belongs_to :friend, class_name: 'User', foreign_key: 'friend_id'
 	
 	# def change
 	# 	create_table :user_friendships do |t|
--- test/models/user_friendship_test.rb
+++ test/models/user_friendship_test.rb
@@ -2,4 +2,11 @@ require 'test_helper'
 
 class UserFriendshipTest < ActiveSupport::TestCase
   should belong_to(:user)
+  should belong_to(:friend)
+
+  test "that creating a friendship works without raising an exception" do
+  	assert_nothing_raised do
+		UserFriendship.create user: users(:isaac), friend: users(:jack)
+	end
+  end
 end
