cf3a41b4e08b852987cc5c1efb0bbbf040ef235d
Isaac Williams <isaac@thewilliams.ws>
2014-04-16 17:10:38 -0400

    Add user's friends index page and do more testing

--- app/controllers/user_friendships_controller.rb
+++ app/controllers/user_friendships_controller.rb
@@ -1,5 +1,10 @@
 class UserFriendshipsController < ApplicationController
-	before_filter :authenticate_user!, only: [:new]
+	before_filter :authenticate_user!
+
+	def index
+		@user_friendships = current_user.user_friendships.load
+	end
+
 
 	def new
 		if params[:friend_id]
--- test/controllers/user_friendships_controller_test.rb
+++ test/controllers/user_friendships_controller_test.rb
@@ -1,6 +1,50 @@
 require 'test_helper'
 
 class UserFriendshipsControllerTest < ActionController::TestCase
+	context "#index" do
+		context "when not logged in" do
+			should "redirect to the login page" do
+				get :index
+				assert_response :redirect
+			end
+		end
+
+		context "when logged in" do
+			setup do
+				@friendship1 = create(:pending_user_friendship, user: users(:isaac), friend: create(:user, first_name: 'Pending', last_name: 'Friend'))
+				@friendship2 = create(:accepted_user_friendship, user: users(:isaac), friend: create(:user, first_name: 'Active', last_name: 'Friend'))
+
+				sign_in users(:isaac)
+				get :index
+			end
+
+			should "get the index page without error" do
+				assert_response :success
+			end
+
+			should "assign user_friendships" do
+				assert assigns(:user_friendships)
+			end
+
+			should "display friend's names" do
+				assert_match /Pending/i, response.body
+				assert_match /Active/i, response.body
+			end
+
+			should "display pending information on a pending friendship" do
+				assert_select "#user_friendship_#{@friendship1.id}" do
+					assert_select "em", "Friendship is pending."
+				end
+			end
+
+			should "display date information on an accepted friendship" do
+				assert_select "#user_friendship_#{@friendship2.id}" do
+					assert_select "em", "Friendship started #{@friendship2.updated_at}."
+				end
+			end
+		end
+	end
+
 	context "#new" do
 		context "when not logged in" do
 			should "redirect to the login page" do
