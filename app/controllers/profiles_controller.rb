5d313ed747670b3ff2effc5d70d8ce7712b2edc5
Isaac Williams <isaac@thewilliams.ws>
2014-04-21 10:52:49 -0400

    Add mailer functionality, reverse order of statuses on profile page, and extras

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
   	@user = User.find_by_profile_name(params[:id])
     @user_friendship = current_user && current_user.user_friendships.find_by_friend_id(@user.id)
   	if @user
-  		@statuses = @user.statuses
+  		@statuses = @user.statuses.order('created_at DESC')
   		render action: :show
   	else
   		render file: 'public/404', status: 404, formats: [:html]
--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module Treebook
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.initialize_on_precompile = false
+
+    config.action_mailer.default_url_options = { host: 'antisocialapp.herokuapp.com' }
   end
 end
