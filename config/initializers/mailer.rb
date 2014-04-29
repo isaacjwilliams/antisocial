4cd1d782fb3c5845c1e1567571102bcb02226ef2
Isaac Williams <isaac@thewilliams.ws>
2014-04-29 17:34:10 -0400

    Remove mailer.rb from initializers

--- config/initializers/mailer.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'antisocialapp.herokuapp.com',
-  :authentication => :plain,
-}
-ActionMailer::Base.delivery_method = :smtp
