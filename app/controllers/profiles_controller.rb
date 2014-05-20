f7f2964ef73be3b53ebc6a9904ff14594fdeda25
Isaac Williams <isaac@thewilliams.ws>
2014-05-20 18:39:38 -0400

    Added pagination to users' profile pages

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,7 @@ class ProfilesController < ApplicationController
   	@user = User.find_by_profile_name(params[:id])
     @user_friendship = current_user && current_user.user_friendships.find_by_friend_id(@user.id)
   	if @user
-  		@statuses = @user.statuses.order('created_at DESC')
+  		@statuses = @user.statuses.order('created_at DESC').paginate(:page => params[:page], :per_page => 15)
   		render action: :show
   	else
   		render file: 'public/404', status: 404, formats: [:html]
